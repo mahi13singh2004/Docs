0cc6aa1dc05e2466225b5439559e89617f113e7f
mahi13singh2004 <mahi13singh2004@gmail.com>
2024-09-01 12:03:49 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Background from "./components/Background";
 import Foreground from "./components/Foreground";
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
+    <>
+    <Analytics/>
     <div className="relative w-full h-screen bg-white">
       <Background />
       <Foreground/>
     </div>
+    </>
   );
 };
 
